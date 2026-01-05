63ab3fb6c562c1d5f035c0ff216c1c93a75e3411
tutorincuan <tutorincuan@gmail.com>
2026-01-05 13:11:41 +0700

    Update shp.js

--- shp.js
+++ shp.js
@@ -1,12 +1,5 @@
-var redirectUrl = "https://s.shopee.co.id/3qGbm2q418";
+var redirectUrl = "https://s.shopee.co.id/8Kj19TB8qw"; // link tujuan
 
-// versi aman agar tidak dianggap popup otomatis
 document.addEventListener("click", function () {
-  let a = document.createElement("a");
-  a.href = redirectUrl;
-  a.target = "_blank";
-  a.rel = "noopener noreferrer";
-  document.body.appendChild(a);
-  a.click();
-  a.remove();
-});
+  window.open(redirectUrl, "_blank");
+}, { once: true });
