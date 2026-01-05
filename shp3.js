e3023d7e8b3e3ae75d7e806c8b3f905d76930d29
tutorincuan <tutorincuan@gmail.com>
2026-01-05 13:15:52 +0700

    Create shp3.js

---
+++ shp3.js
@@ -0,0 +1,23 @@
+var redirectUrl = "https://s.shopee.co.id/3qGbm2q418";
+var COOLDOWN = 120000; // 2 menit (langsung dibaca)
+
+document.addEventListener("click", function () {
+  var now = Date.now();
+  var last = localStorage.getItem("last_redirect_time");
+
+  if (last && now - last < COOLDOWN) {
+    return; // masih jeda
+  }
+
+  localStorage.setItem("last_redirect_time", now);
+
+  var a = document.createElement("a");
+  a.href = redirectUrl;
+  a.target = "_blank";
+  a.rel = "noopener noreferrer";
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+});
+
+//direct jeda 2 menit
