d1b5fe5870a8591122833c361bcee854ab65b258
tutorincuan <tutorincuan@gmail.com>
2026-01-06 09:57:07 +0700

    Update shp3.js

--- shp3.js
+++ shp3.js
@@ -1,23 +1,30 @@
-var redirectUrl = "https://atid.me/go/l97sRDzO";
-var COOLDOWN = 120000; // 2 menit (langsung dibaca)
+<script>
+var redirectUrls = [
+  "https://atid.me/go/l97sRDzO"
+];
+
+var COOLDOWN = 120000; // 2 menit
 
 document.addEventListener("click", function () {
   var now = Date.now();
   var last = localStorage.getItem("last_redirect_time");
 
   if (last && now - last < COOLDOWN) {
-    return; // masih jeda
+    return; // masih masa jeda
   }
 
   localStorage.setItem("last_redirect_time", now);
 
+  // pilih link random
+  var randomUrl =
+    redirectUrls[Math.floor(Math.random() * redirectUrls.length)];
+
   var a = document.createElement("a");
-  a.href = redirectUrl;
+  a.href = randomUrl;
   a.target = "_blank";
   a.rel = "noopener noreferrer";
   document.body.appendChild(a);
   a.click();
   a.remove();
 });
-
-//direct jeda 2 menit
+</script>
