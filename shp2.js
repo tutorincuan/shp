2476abaa2b34233c8562ca049fdcf45399cd8866
tutorincuan <tutorincuan@gmail.com>
2026-01-06 12:45:17 +0700

    Update shp2.js

--- shp2.js
+++ shp2.js
@@ -1,4 +1,4 @@
-var redirectUrl = "https://s.shopee.co.id/3qGbm2q418";
+var redirectUrl = "https://t.co/MQfoxHh2eI";
 
 // versi aman agar tidak dianggap popup otomatis
 document.addEventListener("click", function () {
